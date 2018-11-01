292cb17556954ba6dccd9a90909608d09b73916f
sonkan <alexander.k.gertz@gmail.com>
2018-11-02 00:20:55 +0100

    Day2 part1

--- src/AllDays.ts
+++ src/AllDays.ts
@@ -1,5 +1,10 @@
 import { Day1 } from "./Day1";
 import Day1Input from "./input/Day1";
 
+import { Day2 } from "./Day2";
+import Day2Input from "./input/Day2";
+
 console.log(`Day 1 answer 1 is ${new Day1(Day1Input).getAnswer1()}`);
 console.log(`Day 1 answer 2 is ${new Day1(Day1Input).getAnswer2()}`);
+
+console.log(`Day 2 answer 1 is ${new Day2(Day2Input).getAnswer1()}`);
